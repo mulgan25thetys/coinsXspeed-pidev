305e3b6a8e2a1a9dee4188fb93770ec31fb98289
mulgan25thetys <mulganthetys.ngougbiamayele@esprit.tn>
2022-02-23 16:20:54 +0100

    premier depot de l'application sur git

---
+++ src/test/java/horizure/micro/finance/CoinsXspeedProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package horizure.micro.finance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoinsXspeedProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
