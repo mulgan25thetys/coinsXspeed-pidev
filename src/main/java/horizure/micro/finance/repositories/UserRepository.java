5dd6a0c3fc005ed011e4ada12d48d682af1b7904
omarmezlini <omar.mezlini@esprit.tn>
2022-03-30 20:21:35 +0200

    UserController

--- src/main/java/horizure/micro/finance/repositories/UserRepository.java
+++ src/main/java/horizure/micro/finance/repositories/UserRepository.java
@@ -1,16 +1,12 @@
 package horizure.micro.finance.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import org.springframework.stereotype.Repository;
+
+import org.springframework.data.repository.CrudRepository;
 
 import horizure.micro.finance.entities.User;
 
 @Repository
-public interface UserRepository extends JpaRepository<User,Long> {
 
-	
-	User findByUserName(String userName);
-
-
+public interface UserRepository extends CrudRepository<User, Long> {
 }
